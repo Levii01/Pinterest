f6ba4c8c24ec0c17beb09fd28227148ec6ffdf54
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-01-10 16:15:45 +0100

    Fix aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,10 +85,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'rubypinterest.herokuapp.com' }
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-  :bucket => ENV['AWS_BUCKET'],
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    },
+  url: ':s3_domain_url',
+  path: '/:class/:attachment/:id_partition/:style/:filename',
+  s3_host_name: 's3-eu-west-1.amazonaws.com',
   }
 end
